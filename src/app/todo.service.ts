8780b117ef5a4feba1299d8d8e80eeb4d16ce872
bianca-rodrigues <biancapereirarodrigues02@gmail.com>
2022-12-13 08:37:02 -0300

    service

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ToDo } from './components/todo/todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  private readonly API = "https://jsonplaceholder.typicode.com/todos";
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<ToDo[]>{
+    return this.http.get<ToDo[]>(this.API);
+  }
+
 }
